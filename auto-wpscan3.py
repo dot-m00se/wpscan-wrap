0352afc22b10da24214fc8ca8007b635fbcb6bde
root <root@localhost.localdomain>
2017-01-27 08:08:47 +0200

    Ported to Python3 & added multithreading

--- auto-wpscan3.py
+++ auto-wpscan3.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python
+import multiprocessing
 import traceback
 from subprocess import *
 import shlex
 import re
 from datetime import datetime
 import configparser
-import os
+import os, sys
 
 config = configparser.ConfigParser()
 config.read(os.path.join(os.path.dirname(__file__), 'settings.ini'))
@@ -14,33 +15,35 @@ wp_sites = config.items('wp_sites_to_scan')
 false_positive_strings = config.items('fp')
 log_file = r'./wpwatcher.log'
 
+#Main function that runs wpscan against sites and writes results to logfile
+def main(sites):
+    print("[INFO] Starting scans on configured sites")
 
-def main():
-    print "[INFO] Starting scans on configured sites"
-    for key, sites in wp_sites:
+    sites = sites.strip()
 
+    try:
+       print("[INFO] Scanning '%s'" % sites)
+       result = Popen(['wpscan', '--url', sites, '--follow-redirection'], stdout=PIPE, shell=False)
+       output = result.stdout.read()
+       output1 = output.decode("utf-8")
+       print(output1)
+    except CalledProcessError as exc:
+       print("[ERROR]", exc.returncode, exc.output)
+
+    (warnings, alerts) = parse_results(output1)
+    if output:
         try:
-            print "[INFO] Scanning '%s'" % sites
-            result = Popen(['wpscan', '--url', sites], stdout=PIPE, shell=False)
-            output = result.stdout.read()
-            print output
-        except CalledProcessError as exc:
-            print "[ERROR]", exc.returncode, exc.output
+            with open(log_file, 'a') as log:
+                for warning in warnings:
+                    log.write("%s %s WARNING: %s\n" % (get_timestamp(), sites, warning))
+                for alert in alerts:
+                    log.write("%s %s ALERT: %s\n" % (get_timestamp(), sites, alert))
+        except Exception as e:
+            traceback.print_exc()
+            print("[ERROR] Cannot write to log file")
 
-        (warnings, alerts) = parse_results(output)
-        if output:
-            try:
-                with open(log_file, 'a') as log:
-                    for warning in warnings:
-                        log.write("%s %s WARNING: %s\n" % (get_timestamp(), sites, warning))
-                    for alert in alerts:
-                        log.write("%s %s ALERT: %s\n" % (get_timestamp(), sites, alert))
-            except Exception, e:
-                traceback.print_exc()
-                print "[ERROR] Cannot write to log file"
-
-
-def parse_results(output):
+#Parse the results from wpscan and seperate into Warning/Alert
+def parse_results(output1):
     warnings = []
     alerts = []
     warning_on = False
@@ -48,10 +51,10 @@ def parse_results(output):
     last_message = ""
 
     # Parse the lines
-    for line in output.splitlines():
+    for line in output1.splitlines():
 
         # Remove colorization
-        line = re.sub(r'(\x1b|\[[0-9][0-9]?m)', '', line)
+        line = re.sub('(\x1b|\[[0-9][0-9]?m)', '', str(line))
 
         # Empty line = end of message
         if line == "" or line.startswith("[+]"):
@@ -85,20 +88,33 @@ def parse_results(output):
 
     return (warnings, alerts)
 
-
+#Detect faulse positives
 def is_false_positive(string):
-    # False Positive Detection
     for key, fp_string in false_positive_strings:
         if fp_string in string:
             # print fp_string, string
             return 1
     return 0
 
-
+#Time stamp
 def get_timestamp():
     return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     run_scan()
 
 
 if __name__ == '__main__':
-    main()
+
+    with open(sys.argv[1], 'r') as f:
+        hosts = f.readlines()
+
+    p = multiprocessing.Pool(processes=8)
+    p.map_async(main, hosts)
+    p.close()
+    p.join()
+
+    exit()
+
+
+
+
+
